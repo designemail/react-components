3ede53f05d18d2c9400e0a0bdc8acd02302da000
Matthieu Lux <matthieu.lux@protonmail.com>
2020-07-30 17:22:42 +0200

    Init new generic LabelStack component

--- index.ts
+++ index.ts
@@ -454,6 +454,8 @@ export { default as LoaderPage } from './containers/app/LoaderPage';
 export { default as SquireEditor } from './components/editor/SquireEditor';
 export { default as SimpleSquireEditor } from './components/editor/SimpleSquireEditor';
 
+export { default as LabelStack } from './components/labelStack/LabelStack';
+
 export * from './helpers/component';
 
 export * from './components/layout';
